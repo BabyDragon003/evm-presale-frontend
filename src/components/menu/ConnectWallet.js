199202d6e681e07df39bc101ee65ee282840eccb
BabyDragon003 <golddragon01234@gmail.com>
2020-07-14 15:21:52 +0900

    "2020-07-14 15:21"

--- src/components/menu/ConnectWallet.js
+++ src/components/menu/ConnectWallet.js
@@ -1,13 +1,8 @@
+import { useEffect } from "react";
 import { useSigningClient } from "../../context/web3Context";
 import { isEmpty } from "../../utils";
 
 const ConnectWallet = () => {
-  const {
-    web3,
-    walletAddress,
-    loadWeb3,
-    connectWallet,
-    disconnect,
     updateBalances
   } = useSigningClient();
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { LocationProvider } from '@reach/router';
 import App from './App';
