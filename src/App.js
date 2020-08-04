5578827f431063f79f7bd43273f40a07c89c9816
BabyDragon003 <golddragon01234@gmail.com>
2020-08-04 15:21:52 +0900

    "2020-08-04 15:21"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
+import { useEffect } from 'react';
 import { Router, Location, Redirect } from '@reach/router';
 import { } from 'stylis-plugin-rtl';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-import Presale from './pages/App/presale';
-import Admin from './pages/App/admin';
-import ScrollToTopBtn from './components/menu/ScrollToTop';
-import { SingingWeb3Provider } from './context/web3Context';
-
 const PosedRouter = ({ children }) => (
   <Location>
     {({ location }) => (
