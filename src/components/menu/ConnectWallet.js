21fe1624a0e436e7f8081e016b5317cb7f5865c6
BabyDragon003 <golddragon01234@gmail.com>
2020-08-18 15:21:52 +0900

    "2020-08-18 15:21"

--- src/components/menu/ConnectWallet.js
+++ src/components/menu/ConnectWallet.js
@@ -3,12 +3,6 @@ import { useSigningClient } from "../../context/web3Context";
 import { isEmpty } from "../../utils";
 
 const ConnectWallet = () => {
-  const {
-    web3,
-    walletAddress,
-    loadWeb3,
-    connectWallet,
-    disconnect,
     updateBalances
   } = useSigningClient();
 
