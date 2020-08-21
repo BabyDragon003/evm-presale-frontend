a0c9ea9897763bebe210dc13afcc3e060616ddce
BabyDragon003 <golddragon01234@gmail.com>
2020-08-21 15:21:52 +0900

    "2020-08-21 15:21"

--- src/utils.js
+++ src/utils.js
@@ -18,27 +18,6 @@ export const fadeInUp = keyframes`
 export const fadeIn = keyframes`
   0% {
     opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
-`;
-
-export function debounce(func, wait, immediate) {
-  var timeout;
-  return function () {
-    var context = this,
-      args = arguments;
-    clearTimeout(timeout);
-    timeout = setTimeout(function () {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    }, wait);
-    if (immediate && !timeout) func.apply(context, args);
-  };
-}
-
-export function IsSmMobile() {
   return useMediaQuery({ maxWidth: '639px' });
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,17 @@ module.exports = (config, context) => {
       rules: [
         {
           test: /\.s[ac]ss$/i,
+          enforce: 'pre',
+          use: [
+            "style-loader",
+            "css-loader",
+            {
+              loader: "sass-loader",
+              options: {
+                // Prefer `dart-sass`
+                implementation: require("sass"),
+              },
+            },
             "source-map-loader"
           ],
         },
